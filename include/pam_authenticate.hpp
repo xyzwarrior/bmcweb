e239cda884cb2f87e79fc42342fb32b0a68b2b3c
Steven Deng <steven.deng@premioinc.com>
2020-05-15 21:49:21 +0000

    Add shadow user authentication

--- include/pam_authenticate.hpp
+++ include/pam_authenticate.hpp
@@ -6,6 +6,32 @@
 #include <cstring>
 #include <memory>
 
+#include <unistd.h>
+#include <shadow.h>
+#include <pwd.h>
+
+inline int sys_auth_user (const char*username, const char*password)
+{
+  struct passwd*pw;
+  struct spwd*sp;
+  char*encrypted, *correct;
+
+  pw = getpwnam(username);
+  endpwent();
+
+  if (!pw) return 1; //user doesn't really exist
+
+  sp = getspnam(pw->pw_name);
+  endspent();
+  if (sp)
+     correct = sp->sp_pwdp;
+  else
+     correct = pw->pw_passwd;
+
+  encrypted = crypt(password, correct);
+  return strcmp (encrypted, correct) ? 2 : 0;  // bad pw=2, success=0
+}
+
 // function used to get user input
 inline int pamFunctionConversation(int numMsg, const struct pam_message** msg,
                                    struct pam_response** resp, void* appdataPtr)
